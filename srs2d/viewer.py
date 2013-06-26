eb0ae81bcf7802050858bba80e51e9d0e729c2ef
Eduardo L. Buratti <elb09@c3sl.ufpr.br>
2013-06-26 20:41:51 -0300

    Add GNU licensing and comments to gui/main.py and viewer.py

--- srs2d/viewer.py
+++ srs2d/viewer.py
@@ -1,7 +1,31 @@
-import gtk
-import gui.main
+# -*- coding: utf-8 -*-
+#
+# This file is part of srs2d.
+#
+# srs2d is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# trooper-simulator is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with srs2d. If not, see <http://www.gnu.org/licenses/>.
 
-gtk.gdk.threads_init()
+"""
+Gateway to the viewer (instantiate the Main window).
+"""
+
+__author__ = "Eduardo L. Buratti <eburatti09@gmail.com>"
+__date__ = "26 Jun 2013"
+
+import gui.main
+import logging
+
+__log__ = logging.getLogger(__name__)
 
 if __name__=="__main__":
     gui.main.Main()
