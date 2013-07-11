933404c6991668eb858785d976ea385e5f9c5ff6
Eduardo L. Buratti <elb09@c3sl.ufpr.br>
2013-07-11 08:38:30 -0300

    Fix PSO and add multiprocessing

--- srs2d/pso.py
+++ srs2d/pso.py
@@ -25,6 +25,7 @@ import logging
 import physics
 import robot
 import copy
+import multiprocessing
 
 __log__ = logging.getLogger(__name__)
 
@@ -61,17 +62,26 @@ W = 0.9
 ALFA = 2
 BETA = 2
 
+SIMULATION_DURATION = 600
+NUM_ROBOTS = 30
+D = [1.2, 1.5, 1.9, 2.3, 2.7]
+
+
 class Simulation(object):
-    def __init__(self):
+    def __init__(self, targets_distance=1.2):
+        global NUM_ROBOTS
+
         self.world = physics.World()
 
-        D = [1.2, 1.5, 1.9, 2.3, 2.7]
+        self.targets_distance = targets_distance
+
         H = 4.20
         W = random.uniform(4.20, 4.90)
-        d = D[int(math.floor(random.uniform(0, 4)))]
-        x = math.sqrt(((d / 2.0) ** 2) / 2.0)
+
+        x = math.sqrt(((targets_distance / 2.0) ** 2) / 2.0)
         self.world.add(robot.ColorPadActuator(center=physics.Vector(-x, x), radius=0.27))
         self.world.add(robot.ColorPadActuator(center=physics.Vector(x, -x), radius=0.27))
+        self.full_charge = targets_distance - 2 * 0.27
 
         VERTICAL_WALL_VERTICES = [ (-0.01, H/2.0), (0.01, H/2.0),
                                    (0.01, -H/2.0), (-0.01, -H/2.0) ]
@@ -95,10 +105,17 @@ class Simulation(object):
         wall.add_shape(physics.PolygonShape(vertices=HORIZONTAL_WALL_VERTICES))
         self.world.add(wall)
 
-        NUM_ROBOTS = 12
         self.robots = [robot.Robot(position=physics.Vector( random.uniform(-W/2.0+0.12,W/2.0-0.12), random.uniform(-H/2.0+0.12, H/2.0-0.12) )) for i in range(NUM_ROBOTS)]
         for rob in self.robots:
+            rob.entered_new_target_area = False
+            rob.last_color_pad = None
+            rob.energy = 1.0 + self.full_charge
+            rob.fitness = 0.0
             self.world.add(rob)
+
+        self.world.connect('color-pad-notify', self.on_color_pad_notify)
+        self.world.connect('prepare', self.on_prepare)
+        self.world.connect('think', self.on_think)
 
     def run(self, seconds):
         start = cur = self.world.clock
@@ -106,45 +123,164 @@ class Simulation(object):
             self.world.step()
             cur = self.world.clock
 
+    def on_color_pad_notify(self, pad):
+        global SIMULATION_DURATION
+
+        if self.world.clock < (SIMULATION_DURATION / 2):
+            return
+
+        for rob in self.robots:
+            x = rob.world_center.x
+            y = rob.world_center.y
+
+            if ((pad.center.x - x) ** 2 + (pad.center.y - y) ** 2) < (pad.radius ** 2):
+                if rob.last_color_pad != pad:
+                    rob.entered_new_target_area = True
+                    rob.last_color_pad = pad
+
+    def on_prepare(self):
+        global SIMULATION_DURATION
+
+        if self.world.clock < (SIMULATION_DURATION / 2):
+            return
+
+        for rob in self.robots:
+            rob.entered_new_target_area = False
+
+    def on_think(self):
+        global SIMULATION_DURATION
+
+        if self.world.clock < (SIMULATION_DURATION / 2):
+            return
+
+        for rob in self.robots:
+            if rob.entered_new_target_area:
+                rob.fitness += rob.energy
+                rob.energy = 1 + self.full_charge
+            else:
+                rob.energy -= (abs(rob.wheels.values[0]) + abs(rob.wheels.values[0])) / (2.0 * 82)
+
+class Worker(multiprocessing.Process):
+    def __init__(self, socket):
+        super(Worker, self).__init__()
+        self.socket = socket
+        self.start()
+
+    def run(self):
+        global SIMULATION_DURATION, D, NUM_ROBOTS
+
+        while True:
+            position = self.socket.recv()
+
+            fit = 0.0
+
+            for d in D:
+                for i in range(3):
+                    sim = Simulation(d)
+
+                    for rob in sim.robots:
+                        rob.controller.load(position)
+
+                    sim.run(SIMULATION_DURATION)
+
+                    for rob in sim.robots:
+                        # A robot takes 2.733333 seconds to move one meter at full speed, so
+                        # ((SIMULATION_DURATION / 2.733333) / d) = maximum number of trips a
+                        # robot can do in SIMULATION_DURATION seconds.
+                        # print rob.fitness
+                        fit += rob.fitness / ((SIMULATION_DURATION / 2.733333) / d)
+
+            self.socket.send(fit / (len(D) * 3 * NUM_ROBOTS))
+
 class PSO(object):
     def __init__(self):
         self.gbest = None
+        self.gbest_fitness = None
         self.particles = []
+
+    def run(self, population_size=16, num_workers=8):
+        print 'PSO Starting...'
+        print '==============='
+
+        self.particles = [ Particle() for i in range(population_size) ]
+        for p in self.particles:
+            socketA, socketB = multiprocessing.Pipe()
+            p.socket = socketA
+            w = Worker(socketB)
+
+        while True:
+            print 'Calculating fitness for each particle...'
+            for p in self.particles:
+                p.socket.send(p.position.export())
+            for p in self.particles:
+                p.fitness = p.socket.recv()
+
+            print 'Updating pbest for each particle...'
+            for p in self.particles:
+                p.update_pbest()
+
+            print 'Updating gbest...'
+            for p in self.particles:
+                if (self.gbest is None) or (p.pbest.fitness > self.gbest.fitness):
+                    self.gbest = p.pbest.copy()
+
+                    print 'Found new gbest: ', str(self.gbest)
+
+            print '-' * 80
+            print 'CURRENT GBEST IS: ', str(self.gbest)
+            print str(self.gbest.position)
+            print '-' * 80
+
+            print 'Calculating new position and velocity for each particle...'
+            for p in self.particles:
+                p.gbest = self.gbest
+                p.update_pos_vel()
 
 class Particle(object):
     def __init__(self):
+        self.id = id(self)
+
+        self.position = PVector(True)
+        self.velocity = PVector(True)
+        self.fitness = 0.0
+
         self.pbest = None
         self.gbest = None
 
-        self.position = PVector()
-        self.velocity = PVector()
+    def __str__(self):
+        return 'Particle(%d, fitness=%.5f)' % (self.id, self.fitness)
 
-    def update(self):
+    def copy(self):
+        p = Particle()
+        p.position = self.position.copy()
+        p.velocity = self.velocity.copy()
+        p.fitness = self.fitness
+        p.pbest = self.pbest
+        p.gbest = self.gbest
+        return p
+
+    def update_pbest(self):
+        if (self.pbest is None) or (self.fitness > self.pbest.fitness):
+            self.pbest = self.copy()
+
+            print '[Particle %d] Found new pbest: %s' % (self.id, str(self.pbest))
+
+    def update_pos_vel(self):
         global W, ALFA, BETA
 
         self.velocity = W * self.velocity + \
-                        ALFA * random.uniform(0, 1.0) * (self.pbest - self.position) + \
-                        BETA * random.uniform(0, 1.0) * (self.gbest - self.position)
+                        ALFA * random.uniform(0, 1.0) * (self.pbest.position - self.position) + \
+                        BETA * random.uniform(0, 1.0) * (self.gbest.position - self.position)
 
         self.position = self.position + self.velocity
 
 class PVector(object):
-    def __init__(self, duplicate=None, randomize=False, weights_boundary=(-5.0, 5.0), bias_boundary=(-5.0, 5.0), timec_boundary=(0, 1.0)):
+    def __init__(self, randomize=False, weights_boundary=(-5.0, 5.0), bias_boundary=(-5.0, 5.0), timec_boundary=(0, 1.0)):
         global INPUT_NEURONS, HIDDEN_NEURONS, OUTPUT_NEURONS
 
         self.weights_boundary = weights_boundary
         self.bias_boundary = bias_boundary
         self.timec_boundary = timec_boundary
-
-        if duplicate is not None:
-            self.weights_boundary = copy.deepcopy(duplicate.weights_boundary)
-            self.bias_boundary = copy.deepcopy(duplicate.bias_boundary)
-            self.timec_boundary = copy.deepcopy(duplicate.timec_boundary)
-            self.weights = copy.deepcopy(duplicate.weights)
-            self.bias = copy.deepcopy(duplicate.bias)
-            self.weights_hidden = copy.deepcopy(duplicate.weights_hidden)
-            self.bias_hidden = copy.deepcopy(duplicate.bias_hidden)
-            self.timec_hidden = copy.deepcopy(duplicate.timec_hidden)
 
         if randomize:
             self.weights = { o: { ih: random.uniform(weights_boundary[0], weights_boundary[1]) for ih in INPUT_NEURONS + HIDDEN_NEURONS } for o in OUTPUT_NEURONS }
@@ -153,7 +289,35 @@ class PVector(object):
             self.bias_hidden = { h: random.uniform(bias_boundary[0], bias_boundary[1]) for h in HIDDEN_NEURONS }
             self.timec_hidden = { h: random.uniform(timec_boundary[0], timec_boundary[1]) for h in HIDDEN_NEURONS }
 
-    def __radd__(self, other):
+        else:
+            self.weights = { o: { ih: 0.0 for ih in INPUT_NEURONS + HIDDEN_NEURONS } for o in OUTPUT_NEURONS }
+            self.bias = { o: 0.0 for o in OUTPUT_NEURONS }
+            self.weights_hidden = { h: { i: 0.0 for i in INPUT_NEURONS } for h in HIDDEN_NEURONS }
+            self.bias_hidden = { h: 0.0 for h in HIDDEN_NEURONS }
+            self.timec_hidden = { h: 0.0 for h in HIDDEN_NEURONS }
+
+    def __str__(self):
+        return str({
+            'weights': self.weights,
+            'bias': self.bias,
+            'weights_hidden': self.weights_hidden,
+            'bias_hidden': self.bias_hidden,
+            'timec_hidden': self.timec_hidden
+        })
+
+    def copy(self):
+        pv = PVector()
+        pv.weights_boundary = copy.deepcopy(self.weights_boundary)
+        pv.bias_boundary = copy.deepcopy(self.bias_boundary)
+        pv.timec_boundary = copy.deepcopy(self.timec_boundary)
+        pv.weights = copy.deepcopy(self.weights)
+        pv.bias = copy.deepcopy(self.bias)
+        pv.weights_hidden = copy.deepcopy(self.weights_hidden)
+        pv.bias_hidden = copy.deepcopy(self.bias_hidden)
+        pv.timec_hidden = copy.deepcopy(self.timec_hidden)
+        return pv
+
+    def __add__(self, other):
         if isinstance(other, PVector):
             ret = PVector()
             ret.weights_boundary = self.weights_boundary
@@ -165,8 +329,10 @@ class PVector(object):
             ret.bias_hidden = { h: self.check_boundary(self.bias_boundary, self.bias_hidden[h]+other.bias_hidden[h]) for h in HIDDEN_NEURONS }
             ret.timec_hidden = { h: self.check_boundary(self.timec_boundary, self.timec_hidden[h]+other.timec_hidden[h]) for h in HIDDEN_NEURONS }
             return ret
+        else:
+            raise NotImplemented
 
-    def __rsub__(self, other):
+    def __sub__(self, other):
         if isinstance(other, PVector):
             ret = PVector()
             ret.weights_boundary = self.weights_boundary
@@ -178,8 +344,10 @@ class PVector(object):
             ret.bias_hidden = { h: self.check_boundary(self.bias_boundary, self.bias_hidden[h]-other.bias_hidden[h]) for h in HIDDEN_NEURONS }
             ret.timec_hidden = { h: self.check_boundary(self.timec_boundary, self.timec_hidden[h]-other.timec_hidden[h]) for h in HIDDEN_NEURONS }
             return ret
+        else:
+            raise NotImplemented
 
-    def __rmul__(self, other):
+    def __mul__(self, other):
         if isinstance(other, int) or isinstance(other, float) or isinstance(other, long):
             ret = PVector()
             ret.weights_boundary = self.weights_boundary
@@ -191,7 +359,16 @@ class PVector(object):
             ret.bias_hidden = { h: self.check_boundary(self.bias_boundary, self.bias_hidden[h]*other) for h in HIDDEN_NEURONS }
             ret.timec_hidden = { h: self.check_boundary(self.timec_boundary, self.timec_hidden[h]*other) for h in HIDDEN_NEURONS }
             return ret
+        else:
+            raise NotImplemented
 
+    def __rmul__(self, other):
+        if isinstance(other, int) or isinstance(other, float) or isinstance(other, long):
+            return self.__mul__(other)
+        else:
+            raise NotImplemented
+
+    @staticmethod
     def check_boundary(boundary, value):
         if value < boundary[0]:
             return boundary[0]
@@ -199,6 +376,15 @@ class PVector(object):
             return boundary[1]
         else:
             return value
+
+    def export(self):
+        return {
+            'weights': self.weights,
+            'bias': self.bias,
+            'weights_hidden': self.weights_hidden,
+            'bias_hidden': self.bias_hidden,
+            'timec_hidden': self.timec_hidden
+        }
 
 if __name__=="__main__":
     PSO().run()
