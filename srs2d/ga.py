81dacf79349a10fbec78b62e2297076b6be6166a
Eduardo L. Buratti <elb09@c3sl.ufpr.br>
2013-09-24 16:56:03 -0300

    Update GA

--- srs2d/ga.py
+++ srs2d/ga.py
@@ -46,10 +46,37 @@ POPULATION_SIZE = 120
 
 D = [0.7, 0.9, 1.1, 1.3, 1.5]
 
+PCROSSOVER = 0.9
+PMUTATION = 0.02
+
 class GA(object):
     def __init__(self, context, queue):
         self.context = context
         self.queue = queue
+
+    def select(self):
+        return heapq.heappop(self.population)
+
+    def evaluate(self):
+        for i in xrange(len(self.population)):
+            params = self.population[i][1].decode()
+            self.simulator.set_ann_parameters(i, params)
+        self.simulator.commit_ann_parameters()
+
+        for i in xrange(len(self.population)):
+            self.population[i][0] = .0
+
+        for d in D:
+            for i in range(3):
+                self.simulator.init_worlds(d)
+                self.simulator.simulate()
+
+                fit = self.simulator.get_fitness()
+                for i in xrange(len(self.population)):
+                    self.population[i][0] += fit[i]
+
+        for i in xrange(len(self.population)):
+            self.population[i][0] /= len(D) * 3
 
     def execute(self, run):
         __log__.info(' GA Starting...')
@@ -57,151 +84,158 @@ class GA(object):
 
         run.begin()
 
-        self.population = [ Individual() for i in range(POPULATION_SIZE) ]
+        self.population = [ (0, Genome()) for i in range(POPULATION_SIZE) ]
         self.simulator = physics.Simulator(self.context, self.queue,
                                            num_worlds=POPULATION_SIZE,
                                            num_robots=NUM_ROBOTS,
                                            ta=STEPS_TA, tb=STEPS_TB)
 
+        __log__.info('Calculating fitness for each individual...')
+        self.evaluate()
+
         generation = 0
-
         while (generation < NUM_GENERATIONS):
+            genomeMom = None
+            genomeDad = None
+
+            new_pop = []
+
+            size = len(self.population)
+            if (size % 2) != 0:
+                size -= 1
+
+            for i in xrange(0, , 2):
+                genomeMom = self.select()
+                genomeDad = self.select()
+
+                if (PCROSSOVER >= 1) or (random.random() < PCROSSOVER):
+                   (sister, brother) = genomeMom.crossover(genomeDad)
+
+                if (PMUTATION >= 1) or (random.random() < PMUTATION):
+                    sister.mutate()
+
+                if (PMUTATION >= 1) or (random.random() < PMUTATION):
+                    brother.mutate()
+
+                new_pop.append(sister)
+                new_pop.append(brother)
+
+            if len(self.population) % 2 != 0:
+                last = self.select()
+
+                if (PMUTATION >= 1) or (random.random() < PMUTATION):
+                    last.mutate()
+
+                new_pop.append(last)
+
+            self.population = new_pop
+
             __log__.info('Calculating fitness for each individual...')
-
-            for ind in range(len(self.population)):
-                params = self.population[ind].decode()
-                self.simulator.set_ann_parameters(ind, params)
-            self.simulator.commit_ann_parameters()
-
-            for ind in self.population:
-                ind.fitness = .0
-
-            for d in D:
-                for i in range(3):
-                    self.simulator.init_worlds(d)
-                    self.simulator.simulate()
-
-                    fit = self.simulator.get_fitness()
-                    for ind in range(len(self.population)):
-                        self.population[ind].fitness += fit[ind]
-
-            for i in range(len(self.population)):
-                self.population[i].fitness /= len(D) * 3
-
-            self.population.sort()
-
-            new_population = []
-
-            while len(new_population) < POPULATION_SIZE:
-                if random.random() < CROSSOVER_RATE:
-                    ind1 = self.population.select()
-                    ind2 = self.population.select()
-
-                    offspring = ind1.crossover(ind2)
-
-            for ind in self.population:
-                ind.fitness = .0
-
-            new_gbest = False
-            for p in self.particles:
-                if (self.gbest is None) or (p.pbest.fitness > self.gbest.fitness):
-                    self.gbest = p.pbest.copy()
-                    __log__.info('Found new gbest: %s', str(self.gbest))
-                    new_gbest = True
+            self.evaluate()
 
             generation += 1
 
+            best_genome = max(self.population)[0]
+            new_best = False
+            if (best_genome[0] > last_best_fitness):
+                __log__.info('Found new best genome: %f', best_genome[0])
+                new_best = True
+
+            last_best_fitness = best_genome[0]
+
             __log__.info('-' * 80)
-            __log__.info('[gen=%d] CURRENT GBEST IS: %s', generation, str(self.gbest))
-            __log__.info(str(self.gbest.position))
+            __log__.info('[gen=%d] CURRENT BEST GENOME IS: %f', generation, best_genome[0])
+            __log__.info(str(best_genome[1]))
             __log__.info('-' * 80)
 
-            run.progress(generation / float(NUM_GENERATIONS), {'gbest_fitness': self.gbest.fitness, 'gbest_position': self.gbest.position.to_dict()})
+            run.progress(generation / float(NUM_GENERATIONS), {'best_genome_fitness': best_genome[0], 'best_genome': best_genome[1].decode().to_dict()})
 
-            if new_gbest:
-                __log__.info('Saving simulation for the new found gbest...')
-                self.simulate_and_save('/tmp/simulation.srs', self.gbest.position, D[3])
-                run.upload('/tmp/simulation.srs', 'run-%d-new-gbest-gen-%d.srs' % (run.id, generation) )
+            # if new_best:
+            #     __log__.info('Saving simulation for the new found best genome...')
+            #     self.simulate_and_save('/tmp/simulation.srs', self.gbest.position, D[3])
+            #     run.upload('/tmp/simulation.srs', 'run-%d-new-gbest-gen-%d.srs' % (run.id, generation) )
 
-            for p in self.particles:
-                p.gbest = self.gbest
-                p.update_pos_vel()
+        run.done({'best_genome_fitness': best_genome[0], 'best_genome': best_genome[1].decode().to_dict()})
 
-        run.done({'gbest_fitness': self.gbest.fitness, 'gbest_position': self.gbest.position.to_dict()})
+    # def simulate_and_save(self, filename, pos, distance):
+    #     simulator = physics.Simulator(self.context, self.queue,
+    #                                   num_worlds=1,
+    #                                   num_robots=NUM_ROBOTS,
+    #                                   ta=STEPS_TA, tb=STEPS_TB)
 
-    def simulate_and_save(self, filename, pos, distance):
-        simulator = physics.Simulator(self.context, self.queue,
-                                      num_worlds=1,
-                                      num_robots=NUM_ROBOTS,
-                                      ta=STEPS_TA, tb=STEPS_TB)
+    #     save = io.SaveFile.new(filename, step_rate=1/float(simulator.time_step))
 
-        save = io.SaveFile.new(filename, step_rate=1/float(simulator.time_step))
+    #     simulator.set_ann_parameters(0, pos)
+    #     simulator.commit_ann_parameters()
+    #     simulator.init_worlds(distance)
 
-        simulator.set_ann_parameters(0, pos)
-        simulator.commit_ann_parameters()
-        simulator.init_worlds(distance)
+    #     arena, target_areas, target_areas_radius = simulator.get_world_transforms()
+    #     save.add_square(.0, .0, arena[0][0], arena[0][1])
+    #     save.add_circle(target_areas[0][0], target_areas[0][1], target_areas_radius[0][0], .0, .1)
+    #     save.add_circle(target_areas[0][2], target_areas[0][3], target_areas_radius[0][1], .0, .1)
 
-        arena, target_areas, target_areas_radius = simulator.get_world_transforms()
-        save.add_square(.0, .0, arena[0][0], arena[0][1])
-        save.add_circle(target_areas[0][0], target_areas[0][1], target_areas_radius[0][0], .0, .1)
-        save.add_circle(target_areas[0][2], target_areas[0][3], target_areas_radius[0][1], .0, .1)
+    #     transforms, radius = simulator.get_transforms()
+    #     robot_radius = radius[0][0]
 
-        transforms, radius = simulator.get_transforms()
-        robot_radius = radius[0][0]
+    #     robot_obj = [ None for i in range(len(transforms)) ]
+    #     for i in range(len(transforms)):
+    #         robot_obj[i] = save.add_circle(transforms[i][0], transforms[i][1], robot_radius, transforms[i][2], transforms[i][3])
 
-        robot_obj = [ None for i in range(len(transforms)) ]
-        for i in range(len(transforms)):
-            robot_obj[i] = save.add_circle(transforms[i][0], transforms[i][1], robot_radius, transforms[i][2], transforms[i][3])
+    #     max_steps = STEPS_TA + STEPS_TB
+    #     current_step = 0
+    #     while current_step < max_steps:
+    #         simulator.step()
+    #         transforms, radius = simulator.get_transforms()
+    #         for i in range(len(transforms)):
+    #             robot_obj[i].update(transforms[i][0], transforms[i][1], robot_radius, transforms[i][2], transforms[i][3])
+    #         save.frame()
+    #         current_step += 1
 
-        max_steps = STEPS_TA + STEPS_TB
-        current_step = 0
-        while current_step < max_steps:
-            simulator.step()
-            transforms, radius = simulator.get_transforms()
-            for i in range(len(transforms)):
-                robot_obj[i].update(transforms[i][0], transforms[i][1], robot_radius, transforms[i][2], transforms[i][3])
-            save.frame()
-            current_step += 1
+    #     save.close()
 
-        save.close()
-
-class Particle(object):
+class Genome(object):
     def __init__(self):
         self.id = id(self)
 
-        self.position = physics.ANNParametersArray(True)
-        self.velocity = physics.ANNParametersArray(True)
-        self.fitness = 0.0
+        self.internal = []
 
-        self.pbest = None
-        self.gbest = None
+        self.length = NUM_ACTUATORS * (NUM_SENSORS + NUM_HIDDEN) + (NUM_ACTUATORS) + \
+                        (NUM_HIDDEN * NUM_SENSORS) + NUM_HIDDEN + NUM_HIDDEN
+
+        self.lengths = {
+            'weights': NUM_ACTUATORS * (NUM_SENSORS + NUM_HIDDEN),
+            'bias': NUM_ACTUATORS,
+            'weights_hidden': NUM_HIDDEN * NUM_SENSORS,
+            'bias_hidden': NUM_HIDDEN,
+            'timec_hidden': NUM_HIDDEN,
+        }
 
     def __str__(self):
-        return 'Particle(%d, fitness=%.5f)' % (self.id, self.fitness)
+        return 'Genome(%d)' % (self.id)
 
     def copy(self):
-        p = Particle()
-        p.position = self.position.copy()
-        p.velocity = self.velocity.copy()
-        p.fitness = self.fitness
-        p.pbest = self.pbest
-        p.gbest = self.gbest
-        return p
+        g = Genome()
+        g.internal = self.internal.copy()
+        return g
 
-    def update_pbest(self):
-        if (self.pbest is None) or (self.fitness > self.pbest.fitness):
-            self.pbest = self.copy()
+    def crossover(self, other):
+       """ Single Point crossover """
 
-            __log__.info('[Particle %d] Found new pbest: %s', self.id, str(self.pbest))
+       if len(self.internal) == 1:
+          raise Exception('Cannot crossover genome of length 1.')
 
-    def update_pos_vel(self):
-        global W, ALFA, BETA
+       point = random.randint(1, len(self.internal)-1)
 
-        self.velocity = W * self.velocity + \
-                        ALFA * random.uniform(0, 1.0) * (self.pbest.position - self.position) + \
-                        BETA * random.uniform(0, 1.0) * (self.gbest.position - self.position)
+       sister = self.copy()
+       sister.internal.merge(point, other.internal)
 
-        self.position = self.position + self.velocity
+       brother = other.copy()
+       brother.internal.merge(point, self.internal)
+
+       return (sister, brother)
+
+    def decode(self):
+        return self.internal
 
 if __name__=="__main__":
     uri = os.environ.get('SOLACE_URI')
@@ -219,9 +253,6 @@ if __name__=="__main__":
         'NUM_SENSORS': NUM_SENSORS,
         'NUM_ACTUATORS': NUM_ACTUATORS,
         'NUM_HIDDEN': NUM_HIDDEN,
-        'W': W,
-        'ALFA': ALFA,
-        'BETA': BETA,
         'STEPS_TA': STEPS_TA,
         'STEPS_TB': STEPS_TB,
         'NUM_GENERATIONS': NUM_GENERATIONS,
@@ -232,4 +263,4 @@ if __name__=="__main__":
     })
 
     for run in inst.runs:
-        PSO(context, queue).execute(run)
+        GA(context, queue).execute(run)
