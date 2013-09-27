eb9bec7821d383c11eb0a5f21937f9fc6c9656b3
Eduardo L. Buratti <elb09@c3sl.ufpr.br>
2013-09-27 18:38:56 -0300

    Add creation of scripts to go into /usr/bin

--- setup.py
+++ setup.py
@@ -27,16 +27,23 @@ try:
 except ImportError:
     from distutils.core import setup
 
-setup(name='srs2d',
-      version='0.1',
-      description='Swarm Robotics Simulator 2D',
-      license='GPLv3',
-      author='Eduardo L. Buratti',
-      author_email='eburatti09@gmail.com',
-      url='http://github.com/eburatti09/srs2d',
-      download_url='http://github.com/eburatti09/srs2d',
-      packages=['srs2d'],
-      scripts=[],
-      test_suite='tests',
-      install_requires=[]
-     )
+setup(
+    name='srs2d',
+    version='0.1',
+    description='Swarm Robotics Simulator 2D',
+    license='GPLv3',
+    author='Eduardo L. Buratti',
+    author_email='eburatti09@gmail.com',
+    url='http://github.com/eburatti09/srs2d',
+    download_url='http://github.com/eburatti09/srs2d',
+    packages=['srs2d'],
+    scripts=[],
+    test_suite='tests',
+    install_requires=[],
+    entry_points = {
+        'console_scripts': [
+            'srs2d-pso = srs2d.pso:main',
+            'srs2d-ga = srs2d.ga:main',
+        ],
+    }
+)
