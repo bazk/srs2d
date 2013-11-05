a6a72843d2fa3e3544134cd7c8753df24330c876
Eduardo L. Buratti <elb09@c3sl.ufpr.br>
2013-11-05 14:24:54 -0200

    Update to version 0.2
    Signed-off-by: Eduardo L. Buratti <elb09@c3sl.ufpr.br>

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ except ImportError:
 
 setup(
     name='srs2d',
-    version='0.1',
+    version='0.2',
     description='Swarm Robotics Simulator 2D',
     license='GPLv3',
     author='Eduardo L. Buratti',
