e560cecfd5ead7f1dd44dbdba320ae4e02d06857
Eduardo L. Buratti <elb09@c3sl.ufpr.br>
2013-11-24 08:44:40 -0200

    Add camera[0-3] to save file
    Signed-off-by: Eduardo L. Buratti <elb09@c3sl.ufpr.br>

--- srs2d/physics.py
+++ srs2d/physics.py
@@ -248,6 +248,10 @@ class Simulator(object):
                 actuators1=actuators_hist[0][world][rid][1],
                 actuators2=actuators_hist[0][world][rid][2],
                 actuators3=actuators_hist[0][world][rid][3],
+                camera0=sensors_hist[0][world][rid][8],
+                camera1=sensors_hist[0][world][rid][9],
+                camera2=sensors_hist[0][world][rid][10],
+                camera3=sensors_hist[0][world][rid][11],
                 hidden0=hidden_hist[0][world][rid][0],
                 hidden1=hidden_hist[0][world][rid][1],
                 hidden2=hidden_hist[0][world][rid][2])
